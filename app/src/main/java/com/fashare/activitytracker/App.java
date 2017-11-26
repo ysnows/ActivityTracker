819b053a7d0bb51c2a8397e5163bcad7502098fe
ysnows <ysnows@163.com>
2017-11-26 20:03:55 +0800

    open&close

---
+++ app/src/main/java/com/fashare/activitytracker/App.java
@@ -0,0 +1,16 @@
+package com.fashare.activitytracker;
+
+import android.app.Application;
+
+/**
+ * Created by ysnows on 2017/11/26.
+ */
+
+public class App extends Application {
+    public static boolean isRun = false;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/fashare/activitytracker/MainActivity.java
+++ app/src/main/java/com/fashare/activitytracker/MainActivity.java
@@ -1,5 +1,7 @@
 package com.fashare.activitytracker;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -9,21 +11,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         checkOverlayPermission();
         if (AccessibilityUtil.checkAccessibility(MainActivity.this)) {
-            startService(
-                    new Intent(MainActivity.this, TrackerService.class)
-                            .putExtra(TrackerService.COMMAND, TrackerService.COMMAND_OPEN)
-            );
+            if (App.isRun) {
+                startService(
+                        new Intent(MainActivity.this, TrackerService.class)
+                                .putExtra(TrackerService.COMMAND, TrackerService.COMMAND_CLOSE)
+                );
+                App.isRun = false;
+            } else {
+                startService(
+                        new Intent(MainActivity.this, TrackerService.class)
+                                .putExtra(TrackerService.COMMAND, TrackerService.COMMAND_OPEN)
+                );
+                App.isRun = true;
+            }
             finish();
         }
+    }
+
+
+    /**
+     * 判断服务是否后台运行
+     *
+     * @param mContext Context
+     * @param className 判断的服务名字
+     *
+     * @return true 在运行 false 不在运行
+     */
+    public static boolean isServiceRun(Context mContext, String className) {
+        boolean isRun = false;
+        ActivityManager activityManager = (ActivityManager) mContext
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> serviceList = activityManager
+                .getRunningServices(40);
+        int size = serviceList.size();
+        for (int i = 0; i < size; i++) {
+            if (serviceList.get(i).service.getClassName().equals(className) == true) {
+                isRun = true;
+                break;
+            }
+        }
+        return isRun;
     }
 
     private void checkOverlayPermission() {
